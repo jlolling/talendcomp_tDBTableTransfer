c044f7b2d8e6504e17de9b7a390fedf98b39831c
janlolling <janlolling@0b83e5a7-efa2-48d3-b164-a0b6d2bda42a>
2015-09-22 16:01:36 +0000

    release 2.0

--- talendcomp_tDBTableTransfer/src/de/jlo/talendcomp/tabletransfer/Main.java
+++ talendcomp_tDBTableTransfer/src/de/jlo/talendcomp/tabletransfer/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.cimt.talendcomp.tabletransfer;
+package de.jlo.talendcomp.tabletransfer;
 
 import java.io.File;
 

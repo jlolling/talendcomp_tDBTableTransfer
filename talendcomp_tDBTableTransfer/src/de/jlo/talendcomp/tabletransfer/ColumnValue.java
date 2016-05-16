379d55a02e2acea3340b68ae8c8d0927a7efac8f
janlolling <janlolling@0b83e5a7-efa2-48d3-b164-a0b6d2bda42a>
2016-05-16 18:57:35 +0000

    Release 3.2 removed its own ability to create its own connection. Output method uses for common types the dedicated JDBC methods.

---
+++ talendcomp_tDBTableTransfer/src/de/jlo/talendcomp/tabletransfer/ColumnValue.java
@@ -0,0 +1,27 @@
+package de.jlo.talendcomp.tabletransfer;
+
+public class ColumnValue {
+
+	private String columnName;
+	private Object value;
+	
+	public ColumnValue(String name) {
+		if (name == null || name.trim().isEmpty()) {
+			throw new IllegalArgumentException("name cannot be null or empty!");
+		}
+		this.columnName = name;
+	}
+	
+	public String getColumnName() {
+		return columnName;
+	}
+	
+	public Object getValue() {
+		return value;
+	}
+	
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	
+}
---
+++ talendcomp_tDBTableTransfer/src_test/TalendTest.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TalendTest {
+	
+	
+	protected Map<String, Object> globalMap = new HashMap<String, Object>(); 	
+	protected boolean globalResumeTicket = true;
+	protected String resumeEntryMethodName = null;
+	protected Map<String, Boolean> ok_Hash = new HashMap<String, Boolean>();
+	protected Map<String, Long> start_Hash = new HashMap<String, Long>();
+	protected String currentComponent = null;
+	protected boolean execStat = false;
+
+}
